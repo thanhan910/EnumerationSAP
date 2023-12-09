138966669a2fd0a4b2644893f7d08aba95fdf34e
thanhan910 <63280785+thanhan910@users.noreply.github.com>
2023-12-09 14:53:19 +1100

    Improve test

--- testSAP2D.py
+++ testSAP2D.py
@@ -1,35 +1,18 @@
 import unittest
+import logging
 
 from Enting1980 import G, g, enumerate_saps
 
+OEIS_A002931 = [0, 1, 2, 7, 28, 124, 588, 2938, 15268, 81826, 449572, 2521270, 14385376, 83290424, 488384528, 2895432660, 17332874364, 104653427012, 636737003384, 3900770002646, 24045500114388, 149059814328236, 928782423033008, 5814401613289290, 36556766640745936]
 
 class TestSAP2D(unittest.TestCase):
     def test_enumerate_saps(self):
-        ANS = {
-            4: 1,
-            6: 2,
-            8: 7,
-            10: 28,
-            12: 124,
-            14: 588,
-            16: 2_938,
-            18: 15_268,
-            20: 81_826,
-            22: 449_572,
-            24: 2_521_270,
-            26: 14_385_376,
-            28: 83_290_424,
-            30: 488_384_528,
-            32: 2_895_432_660,
-            34: 17_332_874_364,
-            36: 104_653_427_012,
-            38: 636_737_003_384,
-        }
-        for length, ans in ANS.items():
-            # print success message if test passes, else print error message
+        for length in range(4, 25, 2):
             with self.subTest(length=length):
-                self.assertEqual(enumerate_saps(length), ans)
-
+                ans = enumerate_saps(length)
+                expected_ans = 0 if (length <= 3 or length % 2 != 0) else OEIS_A002931[length // 2]
+                self.assertEqual(ans, expected_ans, msg=f"length={length}, expected_ans={expected_ans}, ans={ans}")
+                
 
 
 if __name__ == "__main__":
